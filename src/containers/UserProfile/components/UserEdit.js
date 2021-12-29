bbaf944ecdc805dd9b814ff7c3f7910a1a62148d
Lucky Saggu <lss007@yahoo.com>
2021-12-29 18:51:47 +0530

    fix console error

--- src/containers/UserProfile/components/UserEdit.js
+++ src/containers/UserProfile/components/UserEdit.js
@@ -60,24 +60,24 @@ const UserEdit = () => {
         <div className="myAccountInner" style={{ maxWidth: '800px',margin:'0 auto', }}>
             <form>
                 <div className="xl:flex xl:justify-between xl:space-x-3 mt-3 px-3">
-                    <div class="flex-1">
+                    <div className="flex-1">
                         <div className="lgTitle">
                             <h3 className="page-title font-medium">Edit User</h3>
                         </div>
                     </div>
                 </div>
                 <div className="xl:flex mt-8 items-center xl:justify-between xl:space-x-3 mt-3 px-3">
-                    <div class="flex-none">
+                    <div className="flex-none">
                         <div className="userInfo">
                             <div className="userImg relative">
                                 <img src={UserImg} alt="User" />
-                                <a href="javascript:void(0)" className="editBtn absolute bg-white">
+                                <a href="#" className="editBtn absolute bg-white">
                                     <img src={EditIcon} alt="Edit" />
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="flex-1 pl-3">
+                    <div className="flex-1 pl-3">
                         <div className="userInfo">                     
                             <div className="userInfoMain">
                                 <h3 className="text-lg text-darkGray font-bold">Jhondoe1</h3>
@@ -87,7 +87,7 @@ const UserEdit = () => {
                     </div>
                 </div>
                 <div className="md:flex mt-6 xl:justify-between md:space-x-6 mt-3 px-3">
-                    <div class="md:flex-1">
+                    <div className="md:flex-1">
                         <div className="formGroup">
                             <label className="block text-sm text-lbGray">
                                 First Name
@@ -102,7 +102,7 @@ const UserEdit = () => {
                             </div>
                         </div>
                     </div>
-                    <div class="md:flex-1 mt-3 md:mt-0">
+                    <div className="md:flex-1 mt-3 md:mt-0">
                         <div className="formGroup">
                             <label className="block text-sm text-lbGray">
                                 Last Name
@@ -119,7 +119,7 @@ const UserEdit = () => {
                     </div>
                 </div>
                 <div className="md:flex mt-3 md:mt-8 xl:justify-between md:space-x-6 mt-3 px-3">
-                    <div class="md:flex-1">
+                    <div className="md:flex-1">
                         <div className="formGroup">
                             <label className="block text-sm text-lbGray">
                                 Display Name
@@ -134,7 +134,7 @@ const UserEdit = () => {
                             </div>
                         </div>
                     </div>
-                    <div class="md:flex-1 mt-3 md:mt-0">
+                    <div className="md:flex-1 mt-3 md:mt-0">
                         <div className="formGroup">
                             <label className="block text-sm text-lbGray">
                                 Email
@@ -151,7 +151,7 @@ const UserEdit = () => {
                     </div>
                 </div>
                 <div className="md:flex mt-3 md:mt-8 xl:justify-between md:space-x-3 mt-3 px-3">
-                    <div class="md:flex-1">
+                    <div className="md:flex-1">
                         <div className="formGroup">
                             <label className="block text-sm text-lbGray">
                                 Wallet Address
@@ -164,7 +164,7 @@ const UserEdit = () => {
                                     className="formControl rounded-lg placeholder-darkGray focus:dropdownTextColor text-sm p-4 block w-full border border-dropdownTextColor"
                                 />
                                 <div className="copyBtn absolute">
-                                    <a href="javascript:void(0)" className="flex">
+                                    <a href="#" className="flex">
                                         <label className="text-stxblue font-medium text-sm pr-2">
                                             Copy
                                         </label>
@@ -176,7 +176,7 @@ const UserEdit = () => {
                     </div>
                 </div>
                 <div className="xl:flex mt-8 xl:justify-between xl:space-x-3 mt-3 px-3">
-                    <div class="flex-1">
+                    <div className="flex-1">
                         <div className="formGroup">
                             <Listbox value={selected} onChange={setSelected}>
                                 {({ open }) => (
@@ -268,7 +268,7 @@ const UserEdit = () => {
                     </div>
                 </div>
                 <div className="xl:flex mt-11 xl:justify-between xl:space-x-3 px-3">
-                    <div class="flex-1">
+                    <div className="flex-1">
                         <div className="formGroup">
                             <button type="submit" className="w-full heightSelect text-center rounded-lg py-3 px-5 bg-stxblue hover:bg-stxblue text-sm text-white font-semibold mb-7">                          
                                 Update User
